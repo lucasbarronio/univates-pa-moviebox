2fce94df17a090f7033df29a6f122809664dc925
Lucas Barronio <lucas.barronio@universo.univates.br>
2026-08-26 19:36:30 -0300

    chore: inicializa projeto Java/Maven

---
+++ src/main/java/br/univates/moviebox/Moviebox.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.univates.moviebox;
+
+/**
+ *
+ * @author lucas
+ */
+public class Moviebox {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
