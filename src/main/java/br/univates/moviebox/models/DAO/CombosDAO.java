f7d3c2996da6145a9e129afa631578284b4962c8
Lucas Barronio <lucas.barronio@universo.univates.br>
2026-09-02 21:10:16 -0300

    refactor: corrige nome do subdiretório de modelos DAO

--- src/main/java/br/univates/moviebox/models/DAO/CombosDAO.java
+++ src/main/java/br/univates/moviebox/models/DAO/CombosDAO.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.univates.moviebox.models.CombosDAO;
+package br.univates.moviebox.models.DAO;
 
 
 import java.sql.ResultSet;
